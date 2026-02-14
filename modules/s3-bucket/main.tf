6d615912313bab50fcc489f2e4dde90ebdbf3b30
giuseppe moffa <giuseppemoff10@gmail.com>
2026-02-14 00:35:43 +0000

    wip

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -52,10 +52,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = var.block_public_access
+  block_public_policy     = var.block_public_access
+  ignore_public_acls      = var.block_public_access
+  restrict_public_buckets = var.block_public_access
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
@@ -99,7 +99,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "default" {
 
   rule {
     id     = "default-lifecycle"
-    status = "Enabled"
+    status = var.enable_lifecycle ? "Enabled" : "Disabled"
 
     filter {}
 
@@ -143,6 +143,18 @@ variable "bucket_name" {
 variable "versioning_enabled" {
   type        = bool
   description = "Enable S3 versioning"
+  default     = true
+}
+
+variable "block_public_access" {
+  type        = bool
+  description = "Block all public access"
+  default     = true
+}
+
+variable "enable_lifecycle" {
+  type        = bool
+  description = "Enable lifecycle config for noncurrent versions and multipart cleanup"
   default     = true
 }
 
